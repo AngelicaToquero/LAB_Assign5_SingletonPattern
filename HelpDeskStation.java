d9f805b2d50d19e9cecdc5c2fe85eb9f40fb508d
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-10-31 01:58:03 +0000

    //

--- HelpDeskStation.java
+++ HelpDeskStation.java
@@ -5,14 +5,11 @@ public class HelpDeskStation {
         this.stationName = stationName;
     }
 
-    // Serve the next person in the queue
     public void serveNext() {
         PagibigQueueSystem queueSystem = PagibigQueueSystem.getInstance();
         queueSystem.incrementQueueNumber();
         System.out.println(stationName + " served a person.");
     }
-
-    // Reset the queue number from this station
     public void resetQueue(int newNumber) {
         PagibigQueueSystem queueSystem = PagibigQueueSystem.getInstance();
         queueSystem.resetQueueNumber(newNumber);
--- Main.java
+++ Main.java
@@ -2,23 +2,16 @@ public class Main {
     public static void main(String[] args) {
         PagibigQueueSystem queueSystem = PagibigQueueSystem.getInstance();
 
-        // Create help desk stations
         HelpDeskStation station1 = new HelpDeskStation("Station 1");
         HelpDeskStation station2 = new HelpDeskStation("Station 2");
         HelpDeskStation station3 = new HelpDeskStation("Station 3");
 
-        // Serve people at different stations
         station1.serveNext();
         station2.serveNext();
         station3.serveNext();
 
-        // Display the current queue number
-        queueSystem.displayQueueNumber();
-
-        // Reset the queue number
-        station1.resetQueue(10);
-
-        // Display the queue number after reset
-        queueSystem.displayQueueNumber();
+        queueSystem.displayQueueNumber();  //display queue
+        station1.resetQueue(10); //reset queue
+        queueSystem.displayQueueNumber(); //display new queue
     }
 }
